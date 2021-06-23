2e5058a414f4c3e75fb9706f0e7626b38847a190
badmodest <blackstile2014@gmail.com>
2021-06-24 00:27:43 +0300

    1

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask import request
-app = Flask(__name__, template_folder='/')
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
 	return render_template('index.html')
-
+	
 if __name__ == '__main__':
     app.run(debug=False)
