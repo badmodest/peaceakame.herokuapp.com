73cacdcd7afc1196e3a477279a3367c3010494d0
badmodest <blackstile2014@gmail.com>
2021-06-24 13:43:17 +0300

    k

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask import request
+
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
 	return render_template('index.html')
-	
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+    	SMS = request.form['SMS']
+    	data = [SMS]
+    	vectorized = cv.transform(data).toarray()
+    	my_prediction = classifier.predict(vectorized)
+    	return render_template('result.html', prediction = my_prediction)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
